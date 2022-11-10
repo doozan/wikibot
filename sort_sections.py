413dabc26f1d67695f4a2dc83f079090e2144542
Jeff Doozan <github@doozan.com>
2022-11-10 17:44:51 -0600

    sort_sections: better logging, support different sorting per language

--- sort_sections.py
+++ sort_sections.py
@@ -2,6 +2,9 @@
 import re
 
 from autodooz.sectionparser import SectionParser
+from autodooz.form_fixer import FormFixer
+from enwiktionary_parser.languages.all_ids import languages as lang_ids
+ALL_LANGS = {v:k for k,v in lang_ids.items()}
 
 # this reads the environment and inits the right locale
 #locale.setlocale(locale.LC_ALL, "")
@@ -97,19 +100,28 @@ def get_language_key(title):
 
     return (2, strip_accents(title))
 
-def sort_languages(parsed):
+def sort_languages(entry):
 
-    if not parsed._children or parsed._children[0].level != 2:
+    if not entry._children or entry._children[0].level != 2:
         return False
 
-    sorted_sections = sorted(parsed._children, key=lambda x: get_language_key(x.title))
-    if sorted_sections == parsed._children:
+    # Don't sort pages with unexpected L2 sections
+    if not all(lang.title in ALL_LANGS for lang in entry._children):
         return False
 
-    parsed._children = sorted_sections
+    sorted_sections = sorted(entry._children, key=lambda x: get_language_key(x.title))
+    if sorted_sections == entry._children:
+        return False
+
+    entry._children = sorted_sections
     return True
 
 def sort_pos(language):
+
+    changes = []
+
+    assert language.title in L3_SORT_LANGUAGES
+    lemmas_before_forms = language.title in L3_LEMMAS_BEFORE_FORMS
 
     sortable = language.filter_sections(matches=lambda x: x.title in COUNTABLE_SECTIONS and x.count)
     if not sortable:
@@ -127,16 +139,36 @@ def sort_pos(language):
             elif c.title in ALL_POS:
                 break
 
-        unhandled = [x.title for x in sort_section._children if
+        unhandled = [x for x in sort_section._children if
                 x.title not in top_sort
                 and x.title not in bottom_sort
                 and x.title not in ALL_POS]
         if unhandled:
-            print("unhandled section, not sorting", unhandled)
-#            # TODO: log these
+#            for section in unhandled:
+#                print("unhandled section, not sorting", section.path)
             continue
 
-        sort_section._children.sort(key=lambda x: get_l3_sort_key(x, alt_first=alt_first))
+
+        # Spanish can sort all of the section in one go
+        if language.title == "Spanish":
+            orig = list(sort_section._children)
+            sort_section._children.sort(key=lambda x: get_l3_sort_key(x, alt_first=alt_first, lemmas_before_forms=lemmas_before_forms))
+            if orig != sort_section._children:
+                changes.append(f"/*{sort_section.path}*/ sorted sections per WT:ELE with forms before lemmas")
+
+        # Since English only sorts a few sections, do it in multiple passes to generate a more verbose summary
+        else:
+            orig = list(sort_section._children)
+            sort_section._children.sort(key=lambda x: get_l3_sort_key_altforms(x, alt_first=alt_first, lemmas_before_forms=lemmas_before_forms))
+            if orig != sort_section._children:
+                changes.append(f"/*{sort_section.path}*/ moved AltForms found before first POS to first section per WT:ELE")
+
+            orig = list(sort_section._children)
+            sort_section._children.sort(key=lambda x: get_l3_sort_key_safe(x, alt_first=alt_first, lemmas_before_forms=lemmas_before_forms))
+            if orig != sort_section._children:
+                changes.append(f"/*{sort_section.path}*/ moved References/Further reading/Anagrams to bottom per WT:ELE")
+
+    return changes
 
 
 
@@ -172,6 +204,7 @@ WT_ELE = {
         #"Derived characters", # not in WT:ELE
         "Related terms",
         #"Related characters", # not in WT:ELE
+        "Collocations",
         "Descendants",
         "Translations",
         #"Statistics", # Not in WT:ELE, but used in 20k pages
@@ -190,7 +223,7 @@ top_sort = {k:v for v,k in enumerate([
         "Etymology",
         "Pronunciation",
         "Production",
-    ])}
+    ], 1)}
 
 #NONSTANDARD_OTHER = {
 #    "Transliteration",
@@ -227,6 +260,7 @@ bottom_sort = {k:v for v,k in enumerate([
         "Derived characters", # not in WT:ELE
         "Related terms",
         "Related characters", # not in WT:ELE
+        "Collocations",
         "Descendants",
         "Translations",
         "Statistics", # Not in WT:ELE, but used in 20k pages
@@ -235,14 +269,27 @@ bottom_sort = {k:v for v,k in enumerate([
         "References",
         "Further reading",
         "Anagrams",
-    ])}
+    ], 1)}
+
+# Categories that can be safely sorted the bottom
+bottom_sort_safe = {k:v for v,k in enumerate([
+        "References",
+        "Further reading",
+        "Anagrams",
+    ], 1)}
 
 ALL_L3_SECTIONS = COUNTABLE_SECTIONS | WT_ELE | ALL_POS.keys() | top_sort.keys() | bottom_sort.keys()
 
-def is_form(section):
-    return bool(re.search(r"{{(head|head-lite)\s*\|[^}]* form", str(section))) or "{{es-past participle}}" in str(section)
+def get_l3_sort_key_altforms(item, alt_first=False, lemmas_before_forms=False):
+    if alt_first and item.title in ["Alternative forms", "Alternative scripts"]:
+        return (0, -1, item.title)
 
-def get_l3_sort_key(item, alt_first=False):
+    return (0,0,0)
+
+def get_l3_sort_key_safe(item, alt_first=False, lemmas_before_forms=False):
+    return (0, 0, bottom_sort_safe.get(item.title, 0))
+
+def get_l3_sort_key(item, alt_first=False, lemmas_before_forms=False):
 
     if alt_first and item.title in ["Alternative forms", "Alternative scripts"]:
         return (0, -1, item.title)
@@ -250,15 +297,19 @@ def get_l3_sort_key(item, alt_first=False):
     if item.title in top_sort:
         sort_group = 0
         sort_class = 0
-        sort_item = str(top_sort[item.title])
+        sort_item = 0 # str(top_sort[item.title])
     elif item.title in ALL_POS:
         sort_group = 1
-        if not is_form(item):
-            sort_class = 0
-            sort_item = 0
-        else:
-            sort_class = 1
-            sort_item = item.title
+        sort_class = 0
+        sort_item = 0
+        if lemmas_before_forms:
+            if not FormFixer.is_form(item):
+                sort_class = 0
+                sort_item = 0  # Lemmas remain in original order
+            else:
+                sort_class = 1
+                sort_item = item.title # Forms sorted a-z
+
     elif item.title in bottom_sort:
         sort_group = 2
         sort_class = 0
@@ -274,6 +325,7 @@ def get_l3_sort_key(item, alt_first=False):
 def sort_l2(text, title, summary, options):
 
     entry = SectionParser(text, title)
+
     old = str(entry).rstrip()
     sort_languages(entry)
     res = str(entry).rstrip()
@@ -285,26 +337,30 @@ def sort_l2(text, title, summary, options):
 
     return res
 
-def es_sort_l3(text, title, summary, options):
+L3_SORT_LANGUAGES = ["Spanish", "English"]
+L3_LEMMAS_BEFORE_FORMS = ["Spanish"]
+
+def sort_l3(text, title, summary, options):
 
     if ":" in title:
         return text
 
     entry = SectionParser(text, title)
-    languages = entry.filter_sections(matches=lambda x: x.title in ["Spanish"], recursive=False)
+    languages = entry.filter_sections(matches=lambda x: x.title in L3_SORT_LANGUAGES, recursive=False)
 
-    changed = False
+    changes = []
     for language in languages:
         old = str(language)
-        sort_pos(language)
-        if str(language) != old:
-            changed = True
+        changes += sort_pos(language)
+#        if str(language) != old:
+#        extra_options = " with lemmas before forms" if language.title in L3_LEMMAS_BEFORE_FORMS else ""
+#            #changes.append(f"/*{language.title}*/ sorted L3 sections per WT:ELE{extra_options}")
+#            changes.append(f"/*{language.title}*/ moved AltForms found before first POS to first section, per WT:ELE")
+#            #changes.append(f"/*{language.title}*/ moved References/Further reading/Anagrams to bottom per WT:ELE")
 
-    if not changed:
+    if not changes:
         return text
 
-    summary.append("/*Spanish*/ Sorted L3 sections per WT:ELE and lemmas before forms")
+    summary += changes
 
     return str(entry).rstrip()
-
-
