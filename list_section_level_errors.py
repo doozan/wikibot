da805abd3b4e3d554ae06f97df4c33d94021ea1a
Jeff Doozan <github@doozan.com>
2022-12-08 16:35:30 -0600

    list_section_x_errors: generate empty pages, added descriptions

--- list_section_level_errors.py
+++ list_section_level_errors.py
@@ -9,27 +9,78 @@ from autodooz.fix_section_levels import SectionLevelFixer
 from autodooz.wikilog import WikiLogger, BaseHandler
 from collections import namedtuple
 
+ALL_FIXES = {
+    "autofix_misplaced_pronunciation": "Misplaced Pronunciation section",
+    "autofix_misplaced_language": "Misplaced Language section",
+    "autofix_misplaced_anagrams": "Misplaced Anagrams section",
+    "autofix_stray_child": "Adoptable stray children",
+    "autofix_unneeded_counter": "Unneeded counter",
+    "autofix_unwanted_children": "Sections that should have no child sections",
+    "autofix_wrong_counter": "Section counter is wrong",
+}
+
+ALL_ERRORS = {
+    "unfinished_state": "Unclosed HTML comment or template",
+    "empty_countable": "Empty countable section",
+    "childless_countable": "Childless countable section",
+    "unexpected_mixed_section": "Unexpected section between known sections",
+    "translation_before_pos": "Translation before first POS section",
+    "embedded_pronunciation_has_children": "Pronunciation with child sections",
+    "non_l2_language": "Language section is not L2",
+    "unexpected_child": "Unexpected child",
+}
+
+ALL_TITLES = ALL_ERRORS | ALL_FIXES
+
 class WikiSaver(BaseHandler):
 
     def sort_items(self, items):
-        return sorted(items, key=lambda x: (x.error, x.page))
+        return sorted(items, key=lambda x: (x.error in ALL_FIXES, x.error, x.page))
 
     def is_new_section(self, item, prev_item):
         return prev_item and prev_item.error != item.error
 
     def is_new_page(self, page_sections, section_entries):
-        return True
+        return page_sections[0][0].error not in ALL_FIXES
 
     def page_name(self, page_sections, prev):
-        return page_sections[0][0].error
+        if page_sections[0][0].error in ALL_FIXES:
+            return "fixes"
+        else:
+            return page_sections[0][0].error
 
-    def page_header(self, base_path, page_name, page_sections, pages):
-        return [f"{sum(map(len, page_sections))} items"]
+    def get_section_header(self, base_path, page_name, section_entries, prev_section_entries, pages):
+        res = []
+        item = section_entries[0]
+        prev_item = prev_section_entries[-1] if prev_section_entries else None
+        if not prev_item or prev_item.error != item.error:
+            title = ALL_TITLES[item.error] if item.error in ALL_TITLES else item.error
+            res.append(f"==={title}===")
+            count = sum(map(len, [x for x in pages[page_name] if x[0].error == item.error]))
+            res.append(f"{count} item{'s' if count>1 else ''}<br>")
+
+        return res
 
     def format_entry(self, entry, prev_entry):
         page = entry.page
         section = entry.section if entry.section else ""
         return [f": [[{page}]] {section} {entry.details}"]
+
+    # Add empty pages if they generated no errors
+    def make_pages(self, *args, **nargs):
+        pages = super().make_pages(*args, **nargs)
+        for error in ALL_ERRORS.keys():
+            if error not in pages:
+                pages[error] = []
+        return pages
+
+    def page_header(self, base_path, page_name, page_sections, pages):
+        if page_sections:
+            return []
+
+        # Generate something for blank pages
+        title = ALL_TITLES[page_name] if page_name in ALL_TITLES else page_name
+        return [f"==={title}===", "0 items"]
 
     _paramtype = namedtuple("params", [ "error", "page", "section", "details" ])
 
@@ -49,10 +100,6 @@ class Logger(WikiLogger):
 
 logger = Logger()
 def log(error, page, section, details):
-    if error == "autofix_stray_child":
-        return
-
-    #print(error, page, section, details)
     logger.add(error, page, section, details)
 
 def main():
@@ -71,7 +118,7 @@ def main():
     parser = dump.parse()
 
     fixer = SectionLevelFixer()
-    fixer.log = log
+    fixer._log = log
 
     count = 0
     for page in parser:
--- list_section_order_errors.py
+++ list_section_order_errors.py
@@ -9,27 +9,68 @@ from autodooz.fix_section_order import SectionOrderFixer
 from autodooz.wikilog import WikiLogger, BaseHandler
 from collections import namedtuple
 
+ALL_FIXES = {
+    "l2_sort": "Unsorted L2 sections",
+    "l3_sort": "Unsorted L3 sections",
+    "pos_sort": "Unsorted POS sections",
+}
+
+ALL_ERRORS = {
+    "dup_sections": "Duplicate L3 sections",
+    "unexpected_child": "Unexpected child section",
+}
+
+ALL_TITLES = ALL_ERRORS | ALL_FIXES
+
 class WikiSaver(BaseHandler):
 
     def sort_items(self, items):
-        return sorted(items, key=lambda x: (x.error, x.page))
+        return sorted(items, key=lambda x: (x.error in ALL_FIXES, x.error, x.page))
 
     def is_new_section(self, item, prev_item):
         return prev_item and prev_item.error != item.error
 
     def is_new_page(self, page_sections, section_entries):
-        return True
+        return page_sections[0][0].error not in ALL_FIXES
 
     def page_name(self, page_sections, prev):
-        return page_sections[0][0].error
+        if page_sections[0][0].error in ALL_FIXES:
+            return "fixes"
+        else:
+            return page_sections[0][0].error
 
-    def page_header(self, base_path, page_name, page_sections, pages):
-        return [f"{sum(map(len, page_sections))} items"]
+    def get_section_header(self, base_path, page_name, section_entries, prev_section_entries, pages):
+        res = []
+        item = section_entries[0]
+        prev_item = prev_section_entries[-1] if prev_section_entries else None
+        if not prev_item or prev_item.error != item.error:
+            title = ALL_TITLES[item.error] if item.error in ALL_TITLES else item.error
+            res.append(f"==={title}===")
+            count = sum(map(len, [x for x in pages[page_name] if x[0].error == item.error]))
+            res.append(f"{count} item{'s' if count>1 else ''}<br>")
+
+        return res
 
     def format_entry(self, entry, prev_entry):
         page = entry.page
         section = entry.section if entry.section else ""
         return [f": [[{page}]] {section} {entry.details}"]
+
+    # Add empty pages if they generated no errors
+    def make_pages(self, *args, **nargs):
+        pages = super().make_pages(*args, **nargs)
+        for error in ALL_ERRORS.keys():
+            if error not in pages:
+                pages[error] = []
+        return pages
+
+    def page_header(self, base_path, page_name, page_sections, pages):
+        if page_sections:
+            return []
+
+        # Generate something for blank pages
+        title = ALL_TITLES[page_name] if page_name in ALL_TITLES else page_name
+        return [f"==={title}===", "0 items"]
 
     _paramtype = namedtuple("params", [ "error", "page", "section", "details" ])
 
