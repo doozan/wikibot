8a276780ef4b672d181342e415d0160b27c6de18
Jeff Doozan <github@doozan.com>
2024-04-20 08:59:54 -0500

    dump_template_use: added --redirects, partial support for dumping section path

--- dump_template_use.py
+++ dump_template_use.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+import csv
 import json
 import multiprocessing
 import mwparserfromhell as mwparser
@@ -10,13 +11,19 @@ import sys
 
 import enwiktionary_sectionparser as sectionparser
 
+from autodooz.escape_template import escape, unescape
 from autodooz.utils import iter_xml, iter_wxt
+
+def clean_name(obj):
+    text = re.sub(r"<!--.*?-->", "", unescape(str(obj.name)), flags=re.DOTALL)
+    return text.strip()
 
 def main():
     parser = argparse.ArgumentParser(description="Find errors in sense lists")
     parser.add_argument("--xml", help="XML file to load")
     parser.add_argument("--wxt", help="Wiktionary extract file to load")
     parser.add_argument("-t", help="template name", action='append')
+    parser.add_argument("--redirects", help="TSV with redirect data")
     #parser.add_argument("-tx", help="template regex", action='append')
     parser.add_argument("--wikiline", help="Dump the entire line containing the template", action='store_true')
     parser.add_argument("--limit", type=int, help="Limit processing to first N articles")
@@ -31,10 +38,20 @@ def main():
     if not args.j:
         args.j = multiprocessing.cpu_count()-1
 
+    redirects = {}
+    if args.redirects:
+        with open(args.redirects) as infile:
+            redirects = {x[0].removeprefix("Template:"):x[1].removeprefix("Template:") for x in csv.reader(infile, delimiter="\t") if x[1].startswith("Template:") and x[1].removeprefix("Template:") in args.t}
+
+    template_names = sorted(args.t + list(redirects.keys()))
+
+#    print("extracting", redirects.keys(), file=sys.stderr)
+#    exit()
+
     if args.wxt:
-        iter_entries = iter_wxt(args.wxt, args.limit, args.progress, args.t, args.wikiline)
+        iter_entries = iter_wxt(args.wxt, args.limit, args.progress, template_names, args.wikiline)
     else:
-        iter_entries = iter_xml(args.xml, args.limit, args.progress, args.t, args.wikiline)
+        iter_entries = iter_xml(args.xml, args.limit, args.progress, template_names, args.wikiline)
 
     if args.j > 1:
         pool = multiprocessing.Pool(args.j)
@@ -42,13 +59,15 @@ def main():
     else:
         iter_items = map(process, iter_entries)
 
+
+
     prev_path = None
     for results in iter_items:
         if not results:
             continue
 
         for entry_title, section_path, template in results:
-            path = f"{entry_title}:{section_path}"
+            path = f"{entry_title}#{section_path}" if section_path is not None else entry_title
             if path != prev_path:
                 print(f"_____{path}_____")
                 prev_path = path
@@ -62,26 +81,44 @@ def process(args):
     if not any(t in entry_text for t in templates):
         return
 
-    wikt = sectionparser.parse(entry_text, entry_title)
-    if not wikt:
-        return
+    if "{{{" in entry_text or "{{#" in entry_text:
+        # Don't treat pages that already contain the 'escape' characters
+        if unescape(entry_text) != entry_text:
+            print(entry_title, "uses escape char", file=sys.stderr)
+            return entry_text
+        entry_text = escape(entry_text, escape_comments=False)
 
-    res = []
-    for section in wikt.ifilter_sections(recursive=False):
+    try:
+        res = []
+        include_section = False
+        if include_section:
+            wikt = sectionparser.parse(entry_text, entry_title)
+            if not wikt:
+                print("unparsable", file=sys.stderr)
+                return
 
-        section_str = str(section)
-        if not any(t in section_str for t in templates):
-            return
+            for section in wikt.ifilter_sections(recursive=False):
 
-        if dump_wikiline:
-            for wikiline in section.content_wikilines:
-                wiki = mwparser.parse(section_str)
-                if any(t for t in wiki.ifilter_templates(matches=lambda x: str(x.name) in templates)):
-                    res.append((entry_title, section.path, wikiline))
+                section_str = str(section)
+                if not any(t in section_str for t in templates):
+                    return
+
+                if dump_wikiline:
+                    for wikiline in section.content_wikilines:
+                        wiki = mwparser.parse(section_str)
+                        if any(t for t in wiki.ifilter_templates(matches=lambda x: clean_name(x) in templates)):
+                            res.append((entry_title, section.path, unescape(wikiline)))
+                else:
+                    wiki = mwparser.parse(section_str)
+                    res += [(entry_title, section.path, unescape(str(t))) for t in wiki.ifilter_templates(matches=lambda x: clean_name(x) in templates)]
         else:
-            wiki = mwparser.parse(section_str)
-            res += [(entry_title, section.path, str(t)) for t in wiki.ifilter_templates(matches=lambda x: str(x.name) in templates)]
+            wiki = mwparser.parse(entry_text)
+            res += [(entry_title, None, unescape(str(t))) for t in wiki.ifilter_templates(matches=lambda x: clean_name(x) in templates)]
 
+    except Exception as e:
+        entry_text, entry_title, *_ = args
+        print("Failed processing", entry_title)
+        raise e
 
     return res
 
