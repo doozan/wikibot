7fbc7bb58b87b48638a499f52b2d35e0f6b44040
Jeff Doozan <github@doozan.com>
2025-01-24 15:16:22 -0300

    fix_fr_tlfi: handle converting old {{R:TLFi}} templates to {{R:fr:TLFi}}

--- fix_fr_tlfi.py
+++ fix_fr_tlfi.py
@@ -1,12 +1,20 @@
 import enwiktionary_sectionparser as sectionparser
+import re
 
 def fr_add_tlfi(text, title, summary, options):
 
     if "==French==" not in text:
         return text
 
-    if "{{R:fr:TLFi" in text or "{{R:TLFi" in text:
+    if "{{R:fr:TLFi" in text:
         return text
+
+    if "{{R:TLFi" in text:
+        new_text = re.sub("({{)R:TLFi\s*([|}])", r"\1R:fr:TLFi\2", text)
+        if new_text != text:
+            if summary is not None:
+                summary.append("renamed {{R:TLFi}} to {{R:fr:TLFi}}")
+            return new_text
 
     entry = sectionparser.parse(text, title)
     if not entry:
@@ -22,19 +30,20 @@ def fr_add_tlfi(text, title, summary, options):
     all_further_reading = french.filter_sections(matches="Further reading")
     if all_further_reading:
         further_reading = all_further_reading[0]
-        further_reading.content_wikilines.insert(0, "* {{R:fr:TLFi}}")
 
     # Otherwise, create and insert or append the new section
     else:
         further_reading = sectionparser.Section(entry, 3, "References")
-        further_reading.content_wikilines.append("* {{R:fr:TLFi}}")
 
+        # References should be inserted before Anagrams if it exists
         anagrams = french.filter_sections(matches="Anagrams", recursive=False)
         if anagrams:
             target_pos = french._children.index(anagrams[0])
             french._children.insert(target_pos, further_reading)
         else:
             french._children.append(further_reading)
+
+    further_reading.content_wikilines.insert(0, "* {{R:fr:TLFi}}")
 
     if summary is not None:
         summary.append("French: added missing TLFi link")
