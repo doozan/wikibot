cfbbe46742d28b60c47df4e776bc5c7fd0d0dd57
Jeff Doozan <github@doozan.com>
2025-09-15 17:13:18 -0300

    handle refs, other updates

--- section_to_links.py
+++ section_to_links.py
@@ -26,13 +26,12 @@ def get_aliases(*templates):
         aliases += _GET_ALIASES.get(template, [])
     return list(set(aliases))
 
-TITLE_TEMPLATES = get_aliases("qualifier")
 TOP_TEMPLATES = get_aliases("box-top", "der-top", "rel-top")
 LINK_TEMPLATES = get_aliases("link", "mention", "ll", "l-lite", "seeSynonyms", "see")
 #LANG_LINK_TEMPLATES = get_aliases("zh-l", "vi-l", "he-l", "ja-r", "ko-l", "th-l", "ja-l")
 LANG_LINK_TEMPLATES = []
 Q_TEMPLATES = get_aliases("qualifier")
-SENSE_TEMPLATES = get_aliases("sense", "sense-lite")
+SENSE_TEMPLATES = get_aliases("sense", "sense-lite", "antsense")
 
 # kinda hacky - global variable overridden by call to section_to_list() that passes a variable
 # to collect parser warnings
@@ -95,13 +94,22 @@ def brackets_to_links(lang_id, text):
 
 
 
-def get_link(lang_id, section, line):
+def get_link(lang_id, section, line, refs):
     """
     Returns a string
     return None if item cannot be parsed
     """
     item = None
     params = {}
+
+    def handle_refs(m):
+        if "ref" not in params:
+            params["ref"] = []
+        params["ref"].append(refs[int(m.group(1))])
+        return ""
+
+    # convert ##REFS## placeholders to ref params
+    line = re.sub(r"##REF(\d+)##", handle_refs, line)
 
     #text = bare_text_to_link(lang_id, line)
     text = brackets_to_links(lang_id, line)
@@ -168,6 +176,8 @@ def get_link(lang_id, section, line):
                     warn("l_has_params", section, str(template))
                     return
 
+        # TODO: handle {{w}} as a link-like template
+
         elif template.name.strip() == "g":
             if len(template.params) != 1:
                 warn("g_has_multiple_params", section, str(template))
@@ -198,10 +208,16 @@ def get_link(lang_id, section, line):
 
     if not item:
         warn("no_item", section, line)
+        return
 
     res = [item]
     for k,v in sorted(params.items(), key=lambda x: ({'pos':0, 'g':1, 'alt':2, 't':3, 'tr':4, 'lit':5, 'ts':6, 'id':7, 'sc':8}.get(x[0], 9), x[0], x[1])):
-        if "<" in v or ">" in v or "{" in v:
+        if k == "ref":
+            if len(v) > 1:
+                warn("multi_ref", section, line)
+                return
+            v = v[0]
+        elif "<" in v or ">" in v or "{" in v:
             warn("bad_parameters", section, v)
             return
         res.append(f"<{k}:{v}>")
@@ -216,13 +232,13 @@ def get_link(lang_id, section, line):
     link = "".join(res)
 
     # strip empty comments
-    link = re.sub("<!--\s*-->", "", link, flags=re.DOTALL)
+    link = re.sub(r"<!--\s*-->", "", link, flags=re.DOTALL)
 
     return link
 
 
 
-def get_links(lang_id, section, line):
+def get_links(lang_id, section, line, refs=[]):
 
     if line.startswith("*"):
         line = line[1:]
@@ -230,8 +246,6 @@ def get_links(lang_id, section, line):
     if not line:
         warn("no_line_data", section, line)
         return
-
-
 
     # Strip "see also" and variants
     # also convert any leading ";" to ","
@@ -243,8 +257,10 @@ def get_links(lang_id, section, line):
 
     line = line.strip(" ,")
 
+
     if "<ref" in line or "{{R:" in line or "{{RQ:" in line:
         warn("has_references", section, line)
+        return
 
     #print("LINE:", line)
 
@@ -252,7 +268,7 @@ def get_links(lang_id, section, line):
         parts = [part for part in nest_aware_split(splitter, line, [("{{", "}}"), ("[[","]]"), ("<", ">")]) if part.strip()]
         if len(parts) > 1:
             #print("PART", parts, [line])
-            links = [get_link(lang_id, section, part) for part in parts if part.strip()]
+            links = [get_link(lang_id, section, part, refs) for part in parts if part.strip()]
             if not links:
                 continue
 
@@ -276,7 +292,7 @@ def get_links(lang_id, section, line):
     if line.startswith("{{col"):
         wikicode = mwparser.parse(line)
         template = next(wikicode.ifilter_templates())
-        if str(template.name).strip() not in ["col", "col2", "col3", "col4", "col5", "col6"]:
+        if str(template.name).strip() not in ["col", "col1", "col2", "col3", "col4", "col5", "col6"]:
             warn("unhandled_column_template", section, line)
             return
 
@@ -284,55 +300,112 @@ def get_links(lang_id, section, line):
             warn("text_outside_col_template", section, line)
             return
 
-        # make sure it's just simple template call without any named params
-        if not all(str(p.name).strip().isdigit() for p in template.params):
-            warn("unhandled_col_params", section, str(template))
-            return
 
+        values = []
         for p in template.params:
+            name = str(p.name).strip()
+
+            if name in ["n", "collapse"]:
+                continue
+
+            if not name.isdigit():
+                # TODO: convert xN values to <x:> inline modifiers
+                warn("unhandled_col_params", section, str(template))
+                return
+
+            if str(name) == "1":
+                continue
+
             val = str(p.value).strip()
+            if not val:
+                continue
+
             if "{{" in val:
                 warn("complex_col_values", section, str(template))
                 return
 
-        return [str(p.value).strip() for p in template.params if str(p.name).strip() != "1" and str(p.value).strip() ]
+            values.append(val)
+
+        return values
 
 
-    link = get_link(lang_id, section, line)
+    link = get_link(lang_id, section, line, refs)
     if link:
         #print("LINK" ,link)
         return [link]
 
 
 
-TITLE_TEMPLATES = Q_TEMPLATES
-RE_TITLE_TEMPLATES = "|".join(sorted(TITLE_TEMPLATES))
-RE_SENSE_TEMPLATES = "|".join(sorted(SENSE_TEMPLATES))
+def _extract_refs(m, refs):
+    name = None
+    if m.group(1).strip():
+        match = re.match(r'''name\s*=\s*(?:"(.*?)"|'(.*?)'|([a-zA-Z\d_-]+))\s*$''', m.group(1))
+        if not match:
+            warn("unhandled ref", section, line)
+            return m.group(0)
+        if match.group(1):
+            name = match.group(1)
+        elif match.group(2):
+            name = match.group(2)
+        else:
+            name = match.group(3)
+
+    data = m.group(2).strip() if len(m.groups())>1 else ""
+
+    if name:
+        refs.append(f"<<name:{name}>>{data}")
+    else:
+        refs.append(data)
+
+    return f"##REF{len(refs)-1}##"
+
+
+# input "test <ref>foo</ref> <ref>bar<ref>"
+# returns ["foo", "bar"], "test ##REF1## ##REF2##"
+def extract_refs(section, line):
+    refs = []
+
+    # <ref name="test"/>
+    line = re.sub(r"<ref\s+([^>]*)\s*/\s*>", lambda x: _extract_refs(x, refs), line)
+    # <ref>blah</ref>
+    # <ref name="test">blah</ref>
+    line = re.sub(r"<ref\s*([^/>]*)>(.*)?<\s*/\s*ref\s*>", lambda x: _extract_refs(x, refs), line)
+
+    return refs, line
+
+
+LABEL_TEMPLATES = get_aliases("label")
+ALL_TITLE_TEMPLATES = sorted(LABEL_TEMPLATES + SENSE_TEMPLATES + Q_TEMPLATES)
+
 def extract_titles(section, line):
 
-    m = re.match(r"^(\*?\s*)\{\{\s*(" + RE_SENSE_TEMPLATES + r")\s*\|", line)
+    m = re.match(r"^(\*?\s*)\{\{\s*(" + "|".join(ALL_TITLE_TEMPLATES) + r")\s*\|", line)
     if m:
 #        print("MATCH", line)
         wikicode = mwparser.parse(line)
         template = next(wikicode.ifilter_templates(), None)
-        assert str(template.name).strip() in SENSE_TEMPLATES, f"bad pattern match {line} {m.group(0)}"
 
+        name = str(template.name).strip()
         if not all(str(p.name).strip().isdigit() for p in template.params):
-            warn("unhandled_sense_params", section, str(template))
+            warn("unhandled_title_params", section, str(template))
 
-        titles = tuple(str(p.value).strip() for p in template.params)
+        if name in LABEL_TEMPLATES:
+            titles = tuple(str(p.value).strip() for p in template.params if str(p.name).strip() != "1" and str(p.value).strip() not in ("", ",", ";", "_"))
+        else:
+            titles = tuple(str(p.value).strip() for p in template.params)
 
-        return titles, line[len(m.group(1))+len(str(template)):]
+        remaining_text = line[len(m.group(1))+len(str(template)):]
+        # strip trailing : after template
+        padding = re.match(r"^\s*:?\s*", remaining_text).group(0)
+        if padding:
+            remaining_text = remaining_text[len(padding):]
+        trailing_semicolon = ":" in padding
 
+        # Q templates only count as titles when followed by a colon as in "Q: foo, bar" or when simply "Q" on its own line
+        if name in Q_TEMPLATES and (remaining_text != "" and not trailing_semicolon):
+            return (None,), line
 
-
-    else:
-#        print("NO MATCH", line)
-
-        # Q templates count as titles when followed by a colon as in "Q: foo, bar" or when simply "Q" on its own line
-        m = re.match(r"\*?\s*\{\{\s*(?:" + RE_TITLE_TEMPLATES + r")\s*[|]([^\|\]}]+)}}(?:\s*:|\s*$)", line)
-        if m:
-            return (m.group(1).strip(),), line[m.end():].strip()
+        return titles, remaining_text
 
     if "|title=" in line:
         wikicode = mwparser.parse(line)
@@ -372,6 +445,9 @@ def section_to_links(section, log=None):
             #print("NO LINE")
             continue
 
+        # Run this before extract_titles to avoid matching "title=" in reference citations
+        refs, line = extract_refs(section, line)
+
         new_titles, line = extract_titles(section, line)
         if new_titles != (None,):
             #print("NEW TITLE", new_title)
@@ -391,7 +467,7 @@ def section_to_links(section, log=None):
 
 
 
-        links = get_links(lang_id, section, line)
+        links = get_links(lang_id, section, line, refs)
         #print(links, line)
         if links is None:
             #print("NO LINK", line)
